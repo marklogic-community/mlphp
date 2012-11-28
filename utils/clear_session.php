06cd1e0ededa27d44e2c5bb09de3a90158a76688
Mike Wooldridge <mike@hellion.org>
2012-11-27 16:28:18 -0800

    Fix relative link back to home page.

--- utils/clear_session.php
+++ utils/clear_session.php
@@ -3,5 +3,5 @@ session_start();
 foreach (array_keys($_SESSION) as $key) {
     unset($_SESSION[$key]);
 }
-echo 'Session variables unset. <a href="index.php">Home</a>'
+echo 'Session variables unset. <a href="../index.php">Home</a>'
 ?>
