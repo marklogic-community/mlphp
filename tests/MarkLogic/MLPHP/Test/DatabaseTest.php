1d4711c1945f27c7b7840510a73fc12c78e2aefc
Mike Wooldridge <mike@hellion.org>
2015-01-15 19:02:17 -0800

    add tests for fields, properties in DatabaseTest

--- tests/MarkLogic/MLPHP/Test/DatabaseTest.php
+++ tests/MarkLogic/MLPHP/Test/DatabaseTest.php
@@ -29,7 +29,11 @@ class DatabaseTest extends TestBase
 
     function setUp() {
       $uri = '/text.xml';
-      $content = '<doc><foo bar="baz">hello</foo><honk sheba="rabies">wipe</honk></doc>';
+      $content = '<doc>
+                    <foo bar="baz">hello</foo>
+                    <one two="3">world</one>
+                    <a b="c">!</one>
+                  </doc>';
       $doc = new MLPHP\Document(parent::$client);
       $doc->setContent($content)->setContentType('application/xml');
       $doc->write($uri);
@@ -86,6 +90,25 @@ class DatabaseTest extends TestBase
         $json = '{"word-searches":false}';
         $properties = $db->setProperties($json)->getProperties();
         $this->assertEquals($properties->{'word-searches'}, false);
+        return $db;
+    }
+
+    function testGetProperty()
+    {
+        parent::$logger->debug('testGetProperty');
+        $db = new MLPHP\Database('mlphp-test', parent::$manageClient);
+        $property = $db->getProperty('database-name');
+        $this->assertEquals($property, 'mlphp-test');
+        return $db;
+    }
+
+    function testSetProperty()
+    {
+        parent::$logger->debug('testSetProperty');
+        $db = new MLPHP\Database('mlphp-test', parent::$manageClient);
+        $key = 'word-searches';
+        $value = $db->setProperty($key, true)->getProperty($key);
+        $this->assertEquals($value, true);
         return $db;
     }
 
@@ -151,6 +174,59 @@ class DatabaseTest extends TestBase
             }
         }
         $this->assertTrue($indexExists);
+        return $db;
+    }
+
+
+    function testAddField()
+    {
+        parent::$logger->debug('testAddField');
+        $db = new MLPHP\Database('mlphp-test', parent::$manageClient);
+        $fieldPath = new MLPHP\FieldPath(
+            array(
+              'path' => 'myPath',
+              'weight' => 1.5
+            )
+        );
+        $included = new MLPHP\FieldElementIncluded(
+            array(
+              'localname' => 'foo'
+            )
+        );
+        $excluded = new MLPHP\FieldElementExcluded(
+            array(
+              'localname' => 'one',
+              'attribute-localname' => 'two',
+              'attribute-value' => 3
+            )
+        );
+        $excluded2 = new MLPHP\FieldElementExcluded(
+            array(
+              'localname' => 'a'
+            )
+        );
+        $field = new MLPHP\Field(array(
+            'field-name' => 'myField',
+            'field-path' => array($fieldPath->properties),
+            'included-element' => array($included->properties),
+            'excluded-element' => $excluded->properties
+        ));
+        print_r($field);
+        $db->addField($field);
+        $response = $db->getResponse();
+        //print_r($response);
+        $properties = $db->getProperties();
+        print_r($properties);
+        // cycle through fields, look for new one
+        $fieldExists = false;
+        print('num fields: ' . count($properties->{'field'}));
+        foreach ($properties->{'field'} as $field) {
+            if ($field->{'field-name'} == 'myField') {
+                $fieldExists = true;
+                break;
+            }
+        }
+        $this->assertTrue($fieldExists);
         return $db;
     }
 
