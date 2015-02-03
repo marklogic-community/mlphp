ef1ad14ddfa987c677c5304d87f7daa9aadbd8eb
Mike Wooldridge <mike@hellion.org>
2015-02-02 19:42:45 -0800

    Update phpunit config file, add bootstrap for setting up API etc. (in progress)

--- phpunit-bootstrap.php
+++ phpunit-bootstrap.php
@@ -1,20 +1,46 @@
 <?php
 
-use Monolog\Logger;
+// use Monolog\Logger;
 
-$loader = require 'vendor/autoload.php';
+// $loader = require 'vendor/autoload.php';
 
-$mlphp = array(
-    'log_level' => Logger::DEBUG,
+// $mlphp = array(
+//     'log_level' => Logger::DEBUG,
 
-    /* 'user'      => 'admin', */
-    /* 'pass'      => 'adm1n', */
+//     /* 'user'      => 'admin', */
+//      'pass'      => 'adm1n',
 
-    /* 'host'      => 'localhost', */
-    /* 'port'      => '8234', */
-    /* 'db'        => 'mlphp-test', */
-    /* 'mgmt_port' => '8002', */
+//     /* 'host'      => 'localhost', */
+//     /* 'port'      => '8234', */
+//     /* 'db'        => 'mlphp-test', */
+//     /* 'mgmt_port' => '8002', */
 
-    'unused'       => foo
-)
+//     'unused'       => foo
+// )
+
+use MarkLogic\MLPHP;
+
+$test_api_name = 'mlphp-test-rest-api';
+$test_db_name = 'mlphp-test';
+
+// Create a REST API for all tests
+$test_api = new MLPHP\RESTAPI(
+    $test_api_name,
+    '127.0.0.1',
+    $test_db_name,
+    8234,
+    'admin',
+    'admin'
+);
+if ($test_api->exists()) {
+    $test_api->delete();
+}
+$test_api->create();
+
+// Delete after all tests complete
+register_shutdown_function(function(){
+    global $test_api;
+    $test_api->delete();
+});
+
 ?>
