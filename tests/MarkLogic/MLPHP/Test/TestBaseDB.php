e38dc1780385c9178cb5427daf0b6709f83f40de
Mike Wooldridge <mike@hellion.org>
2015-02-02 19:41:14 -0800

    in progress: Remove API setup from TestBaseDB

--- tests/MarkLogic/MLPHP/Test/TestBaseDB.php
+++ tests/MarkLogic/MLPHP/Test/TestBaseDB.php
@@ -44,22 +44,22 @@ abstract class TestBaseDB extends TestBase
         parent::setUpBeforeClass();
 
         // Create a REST API for tests
-        self::$api = new MLPHP\RESTAPI(
-            self::$config['apiName'],
-            self::$config['host'],
-            self::$config['db'],
-            self::$config['port'],
-            self::$config['username'],
-            self::$config['password'],
-            parent::$logger
-        );
-        if (self::$api->exists()) {
-            parent::$logger->debug(
-              'REST API ' . self::$config['apiName'] . ' exists, deleting...'
-            );
-            self::$api->delete();
-        }
-        self::$api->create();
+        // self::$api = new MLPHP\RESTAPI(
+        //     self::$config['apiName'],
+        //     self::$config['host'],
+        //     self::$config['db'],
+        //     self::$config['port'],
+        //     self::$config['username'],
+        //     self::$config['password'],
+        //     parent::$logger
+        // );
+        // if (self::$api->exists()) {
+        //     parent::$logger->debug(
+        //       'REST API ' . self::$config['apiName'] . ' exists, deleting...'
+        //     );
+        //     self::$api->delete();
+        // }
+        // self::$api->create();
 
         // Create a REST client for tests
         self::$client = new MLPHP\RESTClient(
@@ -95,9 +95,9 @@ abstract class TestBaseDB extends TestBase
     // https://phpunit.de/manual/current/en/fixtures.html#fixtures.variations
     public static function tearDownAfterClass()
     {
-        self::$api->delete();
-        $db = new MLPHP\Database(self::$config['db'], self::$manageClient);
-        $db->delete();
+        //self::$api->delete();
+        // $db = new MLPHP\Database(self::$config['db'], self::$manageClient);
+        // $db->delete();
     }
 
 }
